49736e7541cd72208652b41bffb119ba987cc71f
Williams Olivera <liamsolivera@gmail.com>
2015-05-04 20:32:06 -0300

    redirect / to /app

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.get('/', function(req, res) {
+  res.redirect('/app/#/home');
+});
 var port = process.env.PORT || 8000;
 app.listen(port, function() {
     console.log('Server listening on port: ' + port);
