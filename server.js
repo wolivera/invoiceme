0d68ff4fed7a6f410e39b2f710e62fe2ce5b5fe8
Williams Olivera <liamsolivera@gmail.com>
2015-05-04 02:49:31 -0300

    changing deploy to node

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/app'));
+app.use('/app', express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 var port = process.env.PORT || 8000;
 app.listen(port, function() {
